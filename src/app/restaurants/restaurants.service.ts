db73e03697155ec26da4c5d7b65a057aba6f5a39
francyglaucio Damasceno Ribeiro <fdamasceno@indracompany.com>
2017-12-13 10:25:54 -0300

    feito ate detalhe do restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -21,4 +21,9 @@ export class RestaurantsService{
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
+    restaurantById(id: String):Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
 }
