698711dc810fe016fb27d05ab2c752e26e45af15
francyglaucio Damasceno Ribeiro <fdamasceno@indracompany.com>
2017-12-14 17:51:11 -0300

    forms primeira parte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RestaurantsComponent } from 'app/restaurants/restaurants.component';
 import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from 'app/restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from 'app/restaurant-detail/reviews/reviews.component';
+import { OrderComponent } from 'app/order/order.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
@@ -15,5 +16,6 @@ export const ROUTES: Routes = [
             {path: 'menu', component: MenuComponent},
             {path: 'reviews', component: ReviewsComponent}
         ]},
+    {path: 'order', component: OrderComponent },
     {path: 'about', component: AboutComponent }
 ]
